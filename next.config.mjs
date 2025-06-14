9f5e56c959922c2f6cb1b059770e25fb09c5e78c
Nikolas Ioannou <nikolas.ioannou@trinomial.xyz>
2025-06-14 18:20:08 +0300

    github action

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export", // Enables static exports
+  reactStrictMode: true,
+  images: {
+    unoptimized: true, // Recommended for GitHub Pages as it doesn't support Next.js image optimization
+  },
+  // If your GitHub Pages URL will be like https://<username>.github.io/<repository-name>/
+  // you'll need to set basePath and assetPrefix. Replace 'your-repo-name' with your actual repository name.
+  basePath: process.env.NODE_ENV === "production" ? "/sofkat" : "",
+  assetPrefix: process.env.NODE_ENV === "production" ? "/sofkat/" : "",
+};
 
 export default nextConfig;
